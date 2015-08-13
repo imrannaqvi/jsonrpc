905c7cc724b52f65d4eeb416d0d4319bf6e57fc4
Imran Naqvi <imrannaqvi83@yahoo.com>
2015-08-14 01:12:05 +0500

    few more basic tests

--- tests/JsonRpcTests/BasicTests.php
+++ tests/JsonRpcTests/BasicTests.php
@@ -1,7 +1,10 @@
 <?php
 namespace JsonRpcTests;
 
+use imrannaqvi\JsonRpc\Server;
 use PHPUnit_Framework_TestCase;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Stdlib\Parameters;
 
 class BasicTests extends PHPUnit_Framework_TestCase
 {
@@ -16,14 +19,32 @@ class BasicTests extends PHPUnit_Framework_TestCase
 	public function testEmptyConfig()
 	{
 		$config = array();
-		$request = new \Zend\Http\PhpEnvironment\Request();
-		$server = new \imrannaqvi\JsonRpc\Server($config, $this->serviceManager);
+		$request = new Request();
+		$server = new Server($config, $this->serviceManager);
 		$response = $server->handle($request);
 		$this->assertArrayHasKey('response', $response);
 		$this->assertNull($response['response']);
 		$this->assertArrayHasKey('error', $response);
 		$this->assertInternalType('string', $response['error']);
+		$this->assertEquals($response['error'], 'method-not-found');
 		$this->assertArrayHasKey('method', $response);
 		$this->assertInternalType('string', $response['method']);
+		$this->assertEquals($response['method'], '');
+	}
+	
+	public function testEmptyConfigWithMethodInRequest()
+	{
+		$method = 'asdf';
+		$config = array();
+		$request = new Request();
+		$request->setMethod(Request::METHOD_POST);
+		$request->setPost(new Parameters(array(
+			'method' => $method
+		)));
+		$server = new Server($config, $this->serviceManager);
+		$response = $server->handle($request);
+		$this->assertArrayHasKey('method', $response);
+		$this->assertInternalType('string', $response['method']);
+		$this->assertEquals($response['method'], $method);
 	}
 }
